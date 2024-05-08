3d07b7be8412802243e0801ce8062209ca3be746
B.Pavel <kiliman6aro@gmail.com>
2024-05-08 11:38:00 +0300

    Add migrations and seeds

---
+++ config/Seeds/ArticlesSeed.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractSeed;
+
+/**
+ * Articles seed.
+ */
+class ArticlesSeed extends AbstractSeed
+{
+
+    public function getDependencies(): array
+    {
+        return [
+            'UsersSeed'
+        ];
+    }
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $data = [
+            'user_id' => 1,
+            'title' => 'First Post',
+            'slug' => 'first-post',
+            'body' => 'This is the first post.',
+            'published' => 1,
+            'created' => date('Y-m-d H:i:s'),
+            'modified' => date('Y-m-d H:i:s'),
+        ];
+
+        $table = $this->table('articles');
+        $table->insert($data)->save();
+    }
+}
